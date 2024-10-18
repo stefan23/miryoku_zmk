b38a465d10edc831d7f11a7e0009ab3ecd062f5d
stefan23 <2190139+stefan23@users.noreply.github.com>
2024-10-18 10:15:50 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,13 +1,2 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-# Uncomment the following lines to enable the Corne RGB Underglow
-CONFIG_ZMK_RGB_UNDERGLOW=y
-CONFIG_WS2812_STRIP=y
-
-# Uncomment the following line to enable the Corne OLED Display
-CONFIG_ZMK_DISPLAY=y
-CONFIG_ZMK_RGB_UNDERGLOW_EFF_START=3
-CONFIG_ZMK_RGB_UNDERGLOW_HUE_STEP=1
-
-&led_strip { chain-length = <27>; };
